dd8951010cd70330250a4c9ce19a465816265c88
KEVINJuegos <keviny030506@gmail.com>
2026-01-18 14:29:19 -0500

    Implemented navigation /// Project restructured

---
+++ Pages/main_menu.py
@@ -0,0 +1,29 @@
+# main_menu.py
+import flet as fl
+
+
+def main_menu(page: fl.Page):
+    page.title = "Main Menu - Noemilator"
+    page.bgcolor = "#121212"
+    page.theme_mode = fl.ThemeMode.DARK
+
+    principal = fl.Column(
+        controls=[
+            fl.Text(value="Menú Principal"),
+            fl.ElevatedButton("New", on_click=lambda _: page.go("/workspace")),
+            fl.ElevatedButton("Settings"),
+            fl.ElevatedButton("Exit", on_click=lambda _: page.window.close()),
+        ],
+        horizontal_alignment=fl.CrossAxisAlignment.CENTER,
+    )
+
+    return fl.View(
+        route="/",
+        controls=[
+            fl.Container(
+                content=principal,
+                expand=True,
+                alignment=fl.alignment.bottom_center,
+            )
+        ],
+    )
--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 # main.py
+import flet as fl
+from Pages.main_menu import main_menu
+from Pages.workspace import workspace
+
+
+def main(page: fl.Page):
+    page.title = "Noemilator"
+
+    def navigator(e):
+        page.views.clear()
+        page.views.append(main_menu(page))
+
+        if page.route == "/workspace":
+            page.views.append(workspace(page))
+
+        page.update()
+
+    def navigator_back(e):
+        page.views.pop()
+
+        top_page = page.views[-1]
+        page.go(top_page.route)  # type:ignore
+
+    page.on_route_change = navigator
+    page.on_view_pop = navigator_back
+
+    page.go("/")  # Launch on main_menu.py
+
+
+fl.app(target=main)
